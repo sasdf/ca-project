c81a8bcd4aab75f806cd15a1c9ffc36e4882d03a
yao11617 <yao11617@gmail.com>
2017-12-08 17:19:49 +0900

    Sign extend ExtOp

--- project1/src/Sign_Extend.v
+++ project1/src/Sign_Extend.v
@@ -1,18 +1,13 @@
 module Sign_Extend(
+    ExtOp_i,
     data_i,
     data_o
 );
 
-// Ports
+input         ExtOp_i;
 input  [15:0] data_i;
 output [31:0] data_o;
 
-// Wires & Registers
-reg    [31:0] data_o;
-
-always@(data_i)
-begin
-    data_o <= { {16{data_i[15]}}, data_i };
-end
+assign data_o = { {16{data_i[15]&ExtOp_i}}, data_i };
 
 endmodule
