9277e9ba42fb2325462e6d7aa388a946095cd17f
yao11617 <yao11617@gmail.com>
2017-12-08 18:14:47 +0900

    Fix ID_EX

--- project1/src/ID_EX.v
+++ project1/src/ID_EX.v
@@ -10,7 +10,9 @@ module ID_EX
     RS_i,
     RT_i,
     SignExtend_i,
-    instr_i,
+    RSAddr_i,
+    RTAddr_i,
+    RDAddr_i,
     RegDst_o,
     ALUSrc_o,
     MemtoReg_o,
@@ -20,7 +22,9 @@ module ID_EX
     RS_o,
     RT_o,
     SignExtend_o,
-    instr_o,
+    RSAddr_o,
+    RTAddr_o,
+    RDAddr_o,
 );
 
 input clk_i;
@@ -34,7 +38,9 @@ input ALUop_i;
 input [31:0] RS_i;
 input [31:0] RT_i;
 input [31:0] SignExtend_i;
-input [25:11] instr_i;
+input [4:0] RSAddr_i;
+input [4:0] RTAddr_i;
+input [4:0] RDAddr_i;
 output RegDst_o;
 output ALUSrc_o;
 output MemtoReg_o;
@@ -44,7 +50,9 @@ output ALUop_o;
 output [31:0] RS_o;
 output [31:0] RT_o;
 output [31:0] SignExtend_o;
-output [25:11] instr_o;
+output [4:0] RSAddr_o;
+output [4:0] RTAddr_o;
+output [4:0] RDAddr_o;
 
 reg RegDst_r;
 reg ALUSrc_r;
@@ -55,7 +63,9 @@ reg ALUop_r;
 reg [31:0] RS_r;
 reg [31:0] RT_r;
 reg [31:0] SignExtend_r;
-reg [25:11] instr_r;
+reg [4:0] RSAddr_r;
+reg [4:0] RTAddr_r;
+reg [4:0] RDAddr_r;
 
 assign RegDst_o = RegDst_r;
 assign ALUSrc_o = ALUSrc_r;
@@ -66,7 +76,9 @@ assign ALUop_o = ALUop_r;
 assign RS_o = RS_r;
 assign RT_o = RT_r;
 assign SignExtend_o = SignExtend_r;
-assign instr_o = instr_r;
+assign RSAddr_o = RSAddr_r;
+assign RTAddr_o = RTAddr_r;
+assign RDAddr_o = RDAddr_r;
 
 always@(posedge clk_i) begin
     RegDst_r <= RegDst_i;
@@ -78,6 +90,8 @@ always@(posedge clk_i) begin
     RS_r <= RS_i;
     RT_r <= RT_i;
     SignExtend_r <= SignExtend_i;
-    instr_r <= instr_i;
+    RSAddr_r = RSAddr_i;
+    RTAddr_r = RTAddr_i;
+    RDAddr_r = RDAddr_i;
 end
 endmodule
